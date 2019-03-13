4452b90674d148d4ac618d9ccd07e1aa6cf3975f
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-03-13 12:45:11 -0500

    Simple animation controller
    still does not stay on one color. cycles between two colors unless we use the any state in between.

---
+++ Simple Animation Controller/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            anim.SetTrigger("MakeRed");
+        else if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("MakeWhite");
+        else if (Input.GetKeyDown(KeyCode.B))
+            anim.SetTrigger("MakeBlack");
+        else if (Input.GetKeyDown(KeyCode.G))
+            anim.SetTrigger("MakeGreen");
+        else if (Input.GetKeyDown(KeyCode.U))
+            anim.SetTrigger("MakeBlue");
+    }
+}
