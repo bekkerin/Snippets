12b0fefe345f73c6a3855496f68d79eb3520c60a
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-03 17:09:59 -0500

    binary files
    unfinished project

---
+++ Binary Files/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public int playerScore;
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        playerScore = data.score;
+        playerName = data.name;
+    }
+ 
+}
---
+++ Binary Files/Assets/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]  //so we can save it to a file
+public class PlayerData 
+{
+    public int score;
+    public string name;
+
+    public PlayerData(Player player)// constructor
+    {
+        score = player.playerScore;//from the player class
+        name = player.playerName;
+    }
+}
