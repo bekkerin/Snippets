3038a9b92b801c3ff184ed182a156f3b943c44f0
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-11 15:17:57 -0500

    Inputfield
    Created

---
+++ InputField/Assets/GetPlayerName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GetPlayerName : MonoBehaviour
+{
+    public InputField PlayerName;
+    public Text ShowName;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayName()
+    {
+        ShowName.text = PlayerName.text;
+    }
+}
