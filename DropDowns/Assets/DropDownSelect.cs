62aefc03e254ec41a06bf0f6eb13e6eb94b788ff
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-11 14:40:08 -0500

    DropDowns
    created

---
+++ DropDowns/Assets/DropDownSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownSelect : MonoBehaviour
+{
+    // this script is attached to the canvas, but could be attached to any gameobject that has an update method.
+    public Dropdown MyDropDown;
+    public Text MyChoice;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(MyDropDown.value)
+        {
+            case 1:
+                MyChoice.text = "Slow";
+                break;
+            case 2:
+                MyChoice.text = "Medium";
+                break;
+            case 3:
+                MyChoice.text = "Fast";
+                break;
+            default:
+                MyChoice.text = "Please Select";
+                break;
+        }
+    }
+}
