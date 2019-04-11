389f139d582d82574cf1fb2f9d4318125f6915f6
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-11 14:56:39 -0500

    slider
    created

---
+++ Sliderbars/Assets/SliderValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderValue : MonoBehaviour
+{
+    public Slider MySlider;
+    public Text ValueOfSlider;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        ValueOfSlider.text = MySlider.value.ToString("f2"); // float with two decimals
+    }
+}
