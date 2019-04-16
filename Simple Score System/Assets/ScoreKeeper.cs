1264dc0c1c1ca0c426206908218b486bef9851b4
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-15 20:22:22 -0500

    simple scoring
    started

---
+++ Simple Score System/Assets/ScoreKeeper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    static public int newScore = 0;
+    public Text NewScore;
+
+    public void IncreaseScore() // clicking on + button increases score
+    {
+        newScore += 1;
+        NewScore.text = newScore.ToString();
+    }
+
+    public void DecreaseScore() // clicking on - button decreases score
+    {
+        newScore -= 1;
+        NewScore.text = newScore.ToString();
+    }
+
+}
---
+++ Simple Score System/Assets/WriteHighScores.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.IO; // for file reading
+using UnityEngine.UI; // for use of UI elements
+using UnityEditor; // so we can update the asset database
+using System;
+
+
+public class WriteHighScores : MonoBehaviour
+{
+    public Text HighScores; // text field showing high scores
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log(ScoreKeeper.newScore); //shows proper transfer of newscore
+        WriteScores();
+    }
+
+    void WriteScores()
+    {
+        string path = "Assets/scores.txt"; // file with the high scores. Initially empty.
+        string[] fields; // fields in each line
+        string playerName; // first field in the fields array
+        int score; // second field in the fields array, need to convert to int with Convert.ToInt32()
+        bool scoreRecorded = false; // we have not written new score to file yet
+ 
+        string[] playerScores = File.ReadAllLines(path); // array of lines in file with name and score
+ 
+        // ReadAllLines automatically closes the file
+
+        StreamWriter writer = new StreamWriter(path); // open the file for writing
+      
+        foreach(string playerScore in playerScores)
+        {
+
+            fields = playerScore.Split(','); // split fields at comma and read into fields array
+            playerName = fields[0]; // first field is name
+            score = Convert.ToInt32(fields[1]); // second field is score
+            if(score < ScoreKeeper.newScore && scoreRecorded == false)
+            {
+                writer.Write("Ernst" + "," + ScoreKeeper.newScore + "\n"); // before we write the existing score, write the new high score
+                HighScores.text += "Ernst" + " : " + score.ToString() + "\n"; // also show to user
+                scoreRecorded = true; 
+            }
+            writer.Write(playerScore + "\n"); // either way, still write the existing score
+            HighScores.text += playerName + " : " + score.ToString() + "\n"; // also show to user
+        }
+
+        writer.Close(); // so we don't lock it for next round
+
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("scores");
+
+    }
+}
