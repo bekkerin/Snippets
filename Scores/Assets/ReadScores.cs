c648f69d3e601f32d319432c6d6ce43494834ca5
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-23 11:27:19 -0500

    rationalized
    old stuff deleted

---
+++ Scores/Assets/ReadScores.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class ReadScores : MonoBehaviour
+{
+    public Text HighScores;
+    public int num_scores = 5;
+
+public void ShowTopScores()
+    {
+        string path = "Assets/scores.txt";
+        string line;
+        string[] fields;
+        string[] playerNames = new string[num_scores];
+        int[] playerScores = new int[num_scores];
+        int scores_read = 0;
+
+        HighScores.text = ""; // clear the scores box
+
+        StreamReader reader = new StreamReader(path);
+        while(!reader.EndOfStream && scores_read < num_scores)
+        {
+            line = reader.ReadLine();
+            fields = line.Split(',');
+            HighScores.text += fields[0] + " : " + fields[1] + "\n";
+            scores_read += 1;
+        }
+
+    }
+}
