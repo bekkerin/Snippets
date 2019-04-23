242869e832d0d3b5eefe068a23867494de78f9a3
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-23 11:23:09 -0500

    scoring
    new

---
+++ Scoring/Assets/ReadScores.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+
+public class ReadScores : MonoBehaviour
+{
+    public Text OldScores;
+    // attach this script to camera so it is executed on load
+    // Start is called before the first frame update
+   public void ReadFile()
+    {
+        string path = "Assets/scores.txt";
+        string playerRecord; // string of lines in file
+        string[] fields;
+        string playerName ; // first field in each line
+        int playerScore ; // second field in each line
+
+        StreamReader sr = new StreamReader(path);
+
+        int x = 0; // keep track of index
+
+        while (!sr.EndOfStream)
+        {
+            playerRecord = sr.ReadLine();
+            fields = playerRecord.Split(',');
+            Debug.Log(playerRecord);
+            playerName = fields[0];
+            playerScore = Convert.ToInt32(fields[1]);
+            Debug.Log(playerName + playerScore.ToString());
+        }
+        sr.Close();
+
+
+    }
+
+}
---
+++ Scoring/Assets/Scoring.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Scoring : MonoBehaviour
+{
+    static public int newScore = 0;
+    public Text NewScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreaseScore() // clicking on + button increases score
+    {
+        newScore += 1;
+        NewScore.text = PlayerName.playerName + ' ' + newScore.ToString();
+    }
+
+    public void DecreaseScore() // clicking on - button decreases score
+    {
+        newScore -= 1;
+        NewScore.text = PlayerName.playerName + ' ' + newScore.ToString();
+    }
+}
