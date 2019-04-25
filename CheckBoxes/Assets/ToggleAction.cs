e942d3a791a747ad1c747b7729fcabe0fa0d6c80
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-04-25 14:33:39 -0500

    sync
    lkjalsdjf

---
+++ CheckBoxes/Assets/ToggleAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleAction : MonoBehaviour
+{
+    public Button MyButton;
+    public Text MyText;
+    public InputField MyInputField;
+    public Slider MySlider;
+    public Toggle MyToggle;
+
+    //need to connect to ToggleAction.Toggle_Changed(), not ToggleAction.Toggle_Changed(bool). 
+    public void Toggle_Changed(bool newValue)
+    {
+        MyButton.enabled = newValue;
+        MyInputField.enabled = newValue;
+        MySlider.interactable = newValue;
+        if(newValue == false)
+        {
+            MyText.text = "checkbox off";
+        }
+        else
+        {
+            MyText.text = "checkbox on";
+        }
+
+
+    }
+
+}
---
+++ Simple Score System/Assets/LivesTracker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class LivesTracker : MonoBehaviour
+{
+    public static int lives = 9;
+    public Text LivesRemaining;
+
+    public void DecreaseLives()
+    {
+        lives -= 1;
+        LivesRemaining.text = lives.ToString();
+        Debug.Log(lives);
+    }
+    public void IncreaseLives()
+    {
+        lives += 1;
+        LivesRemaining.text = lives.ToString();
+        Debug.Log(lives);
+    }
+
+}
