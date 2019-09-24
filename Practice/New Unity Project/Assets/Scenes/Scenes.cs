a9e08fda349d9984f7da7d040077d612ec53d72d
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-09-24 13:53:20 -0500

    dropdowns
    fixed

---
+++ Practice/New Unity Project/Assets/Scenes/Scenes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+        
+    
+
+}
+
+
+
---
+++ sharing data between scenes/Assets/Scenes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void ExitGame()
+    {
+        // exit the game for real play
+        //Application.Quit();
+        //does not work in Unity since it would  lose all settings. so:
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
