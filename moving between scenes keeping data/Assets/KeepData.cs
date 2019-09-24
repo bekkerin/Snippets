e79f7693adc7a8aa482588c89b8ee8e236a4425f
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-09-23 19:26:52 -0500

    added moving between and saving data
    now we can pass data to other scenes

---
+++ moving between scenes keeping data/Assets/KeepData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//THIS SCRIPT IS ATTACHED TO THE DATA MANAGER OBJECT PRESENT IN THE SCENES
+
+public class KeepData: MonoBehaviour 
+{
+    public static string PlayerName; // STATIC so it is preserved between classes
+    public InputField PlayerNameInput;
+    public Text showPlayerName;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject); // don't destroy this game object when a new scene is loaded.
+    }
+
+    public void UpdateName() //executed in each letter change
+    {
+        PlayerName = PlayerNameInput.text; // assign new input text to string
+        Debug.Log(PlayerName); //prints updated name to log on each letter change
+        showPlayerName.text = PlayerName; // show string in the textbox just to see that it works in the Intro scene. 
+    }
+    public void ShowName()
+    {
+        showPlayerName.text = PlayerName;
+        Debug.Log(PlayerName);
+    }
+
+
+
+}
