a7bfe9a884811b49bedbadd58a3fa9c7d71ed484
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-10-03 14:49:32 -0500

    updated
    added frame

---
+++ moving between scenes keeping data/Assets/ShowData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// this script is attached to the camera. It executes when the camera awakes and loads the values in the text boxes on the panel so you don't have to click a button.
+
+public class ShowData : MonoBehaviour
+{
+    public Text PanelNameText;
+    public Text PanelLivesText;
+    public Text PanelSpeedText;
+
+    private void Awake()
+    {
+        PanelNameText.text = "Name: " + KeepData.PlayerName;
+        PanelLivesText.text = "Lives: " + KeepData.PlayerLives.ToString();
+        PanelSpeedText.text = "Speed: " + KeepData.PlayingSpeed.ToString();
+        
+    }
+
+}
