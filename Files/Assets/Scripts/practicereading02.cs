5e540415ba5e4d72d5460ed1d017215fdcdc8075
Ernst Bekkering <bekkerin@users.noreply.github.com>
2019-11-19 13:58:06 -0600

    kfjgl
    dgdg

---
+++ Files/Assets/Scripts/practicereading02.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+
+public class practicereading02 : MonoBehaviour
+{
+    public Text fileContents;
+
+    public void ReadString2()
+    {
+        string path = "Assets/Resources/test.txt";
+
+        fileContents.text = "";
+
+        string[] readText = File.ReadAllLines(path);
+        foreach (string s in readText)
+        {
+            Debug.Log(s);
+            fileContents.text += s + "\n";
+        }
+    }
+}
