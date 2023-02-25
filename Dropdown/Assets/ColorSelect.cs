02775ceeae99ac4881b69343be99d4bdc35993f9
Ernst Bekkering <bekkerin@users.noreply.github.com>
2023-02-24 18:01:08 -0600

    update
    dropdowns and colors update

--- Dropdown/Assets/ColorSelect.cs
+++ Dropdown/Assets/ColorSelect.cs
@@ -6,10 +6,9 @@ using UnityEngine.UI;
 public class ColorSelect : MonoBehaviour
 {
     public Dropdown ColorDropdown;
-    public Dropdown SpeedDropdown;
     public Text ColorText;
     public GameObject PlayerCapsule;
-    public int speed;
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,23 +43,5 @@ public class ColorSelect : MonoBehaviour
                 break;
         }
     }
-    public void ChooseSpeed()
-    {
-        switch (SpeedDropdown.value)
-        {
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            default:
-
-                break;
-        }
-
-    }
+   
 }
--- Dropdown/Assets/PlayerMovement.cs
+++ Dropdown/Assets/PlayerMovement.cs
@@ -16,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() //  runs automatically for the cylinder
     {
-        if (PlayerCapsule.transform.position.x >= 4.0f)
+        if (PlayerCapsule.transform.position.x >= 4.0f) // rightmost of plane is 4.0
         {
             movement = -movement; // was positive to right, now turns negative to left
         }
-        else if (PlayerCapsule.transform.position.x <= -4.0f)
+        else if (PlayerCapsule.transform.position.x <= -4.0f) // left most of plane is -4.0
         {
             movement = -movement; // was negative to left, now turns positive to right
         }
