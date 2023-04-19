89f9d9a3c373b2d710990363b4a9b1d616ebe1a5
Ernst Bekkering <bekkerin@users.noreply.github.com>
2023-04-19 15:20:24 -0500

    added salsa animation

---
+++ Animation/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem; // use for input system
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+		public bool salsa;
+		public bool attack;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+
+		public void OnSalsa(InputValue value)
+		{
+			SalsaInput(value.isPressed);
+		}
+
+		public void SalsaInput(bool newSalsaState)
+		{
+			salsa = newSalsaState;
+		}
+
+		public void OnAttack(InputValue value)
+		{
+
+			AttackInput(value.isPressed);
+
+		}
+
+		public void AttackInput(bool newAttackState)
+		{
+
+			attack = newAttackState;
+
+		}
+	}
+	
+}
